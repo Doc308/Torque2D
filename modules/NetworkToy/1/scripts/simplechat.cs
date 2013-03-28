e70e929bdb17b351d05785ddadb2c3ff8648409b
Doc308 <kirksander@hotmail.com>
2013-03-28 14:45:23 -0500

    Update simplechat.cs

--- modules/NetworkToy/1/scripts/simplechat.cs
+++ modules/NetworkToy/1/scripts/simplechat.cs
@@ -1,7 +1,7 @@
 //How to use this example
 //1. Launch two versions of T2D
 //2. Click StartServer button. You should see a connected message (the server computer connects as a client to itself).
-//3. In the console on your client click JoinServer button. You should see a connected message.
+//3. In the console on your client click JoinServer button. Click QueryLan, then Join Server. You should see a connected message.
 //4. In the console on the server run 'serversayhi("NAME")'. Where NAME is your name or whatever.
 //5. Check the console on the client and you should see "NAME (server) says hi!"
 //6. In the console on the client run 'clientsayhi("NAME")'. Where NAME is your name or whatever. 
